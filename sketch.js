7f53879f5bc918a519063e90781164ae6d77fecd
mypaxa00 <36297589+mypaxa00@users.noreply.github.com>
2018-10-19 17:51:18 +0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,20 +1,55 @@
-var stars = [];
-
-var speed;
+let yoff = 1;
+let nums;
 
 function setup() {
-  createCanvas(1024, 1080, WEBGL);
-  for (var i = 0; i < 800; i++) {
-    stars[i] = new Star();
+  createCanvas(400, 400);
+  let string = "Шифрование";
+  let len = string.length;
+  nums = new Array(256);
+  for (let i = 0; i < len; i++) {
+    nums[i] = string.charCodeAt(i);
   }
+  for (let i = 1; i < nums.length; i++) {
+    x = i % len;
+    nums[i] = ((nums[i - 1] % nums[x]) + nums[x] * nums[i - 1]) % 249 + 7;
+  }
+  nums[0] %= 249 + 7
+  console.log(nums);
 }
 
 function draw() {
-  speed = map(mouseX, 0, width, 0, 50);
-  background(0);
+
   translate(width / 2, height / 2);
-  for (var i = 0; i < stars.length; i++) {
-    stars[i].update();
-    stars[i].show();
+  background(51);
+
+  stroke(255);
+  fill(255, 50);
+  strokeWeight(1);
+
+  let details = nums[0] * 2;
+
+  let xoff = 0;
+  beginShape();
+  for (let a = 0, i = 1; a <= PI; a += PI / details) {
+    let r = sin(2 * a) * map(nums[i], 1, 256, nums[1], nums[2]);
+    let x = r * cos(a) * sin(yoff);
+    let y = r * sin(a);
+    i++;
+    vertex(x, y);
   }
+  vertex(0, 0);
+  endShape();
+  beginShape();
+  for (let a = 0, i = 1; a <= PI; a += PI / details) {
+    let r = sin(2 * a) * map(nums[i], 1, 256, nums[1], nums[2]);
+    let x = r * cos(a) * sin(yoff);
+    let y = r * sin(a);
+    i++;
+    vertex(-x, y);
+  }
+  vertex(0, 0);
+  endShape();
+
+  yoff += 0.15;
+  yoff %= 2 * PI;
 }
